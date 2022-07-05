2476b61fac7bd3fd8046defc3816546e96642e90
stimur1709 <stimur1709@mail.ru>
2022-07-05 15:07:40 +0700

    Initial commit

---
+++ src/main/java/ru/safin/githubparsing/GitHubParsingApplication.java
@@ -0,0 +1,13 @@
+package ru.safin.githubparsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubParsingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubParsingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/safin/githubparsing/GitHubParsingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safin.githubparsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubParsingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
