fa33d0755a8b0db2cc1d1a00fffa7f257f4c36c7
stimur1709 <stimur1709@mail.ru>
2022-07-05 17:37:37 +0700

    реализован запрос списка фолловеров

--- src/main/java/ru/safin/githubparsing/controller/MainController.java
+++ src/main/java/ru/safin/githubparsing/controller/MainController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.safin.githubparsing.model.Follower;
 import ru.safin.githubparsing.service.FollowerService;
-
-import java.util.List;
 
 @Controller
 public class MainController {
--- src/main/java/ru/safin/githubparsing/model/Follower.java
+++ src/main/java/ru/safin/githubparsing/model/Follower.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Follower {
     private String login;
-    private int id;
     private String avatar_url;
     private String html_url;
 
@@ -13,41 +12,11 @@ public class Follower {
         return login;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getAvatar_url() {
         return avatar_url;
     }
 
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
-    }
-
     public String getHtml_url() {
         return html_url;
-    }
-
-    public void setHtml_url(String html_url) {
-        this.html_url = html_url;
-    }
-
-    @Override
-    public String toString() {
-        return "Follower{" +
-                "login='" + login + '\'' +
-                ", id=" + id +
-                ", avatar_url='" + avatar_url + '\'' +
-                ", html_url='" + html_url + '\'' +
-                '}';
     }
 }
